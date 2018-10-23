6dedc2bba13719e53264c4a59c6237ae5b2e3cba
Damian Iglesias <damian.e.iglesias@gmail.com>
2018-10-23 11:02:14 -0300

    added background parallax

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+[0.0.1] - 2018-10-16
+###Added
+	-	Player Character Functionality
+	-	Placeholder PC Asset
+	-	Obstacle Functionality
+	-	Placeholder Obstacle Functionality
+[0.0.2] - 2018-10-23
+###Added
+	- Background Scrolling
--- SideScroller/SideScroller/Main.cpp
+++ SideScroller/SideScroller/Main.cpp
@@ -9,38 +9,28 @@ void Obstacle();
 void gameOver();
 Rectangle rec1;
 Rectangle rec2;
+void Draw();
+void Update();
+void Init();
+Image image;
+Texture2D ImagenFondo;
+Rectangle fondo;
+Rectangle fondo2;
+Rectangle player = {PlayerPos.x, PlayerPos.y, (float)PlayerSize.x, (float)PlayerSize.y};
+Rectangle obstacle = { 0.0f, 0.0f, (float)ImagenFondo.width / 2, (float)ImagenFondo.height };
+Vector2 position1 = { 0.0f, 0.0f };
+Vector2 position2 = { 500.0f, 0.0f };
+Vector2 position3 = { -500.0f, 0.0f };
 int main()
 {
 	// Initialization
 	//--------------------------------------------------------------------------------------
-
-
-	InitWindow(screenWidth, screenHeight, "SideScroll Practice");
-	SetTargetFPS(60);
+	Init();
 	while (!WindowShouldClose() )  
 	{
-		// Update
-		if (IsKeyDown(KEY_UP)) PlayerPos.y -= 2.0f;
-		if (IsKeyDown(KEY_DOWN)) PlayerPos.y += 2.0f;
-		if (PlayerPos.y > 420)
-		{PlayerPos.y = 420;}
-		if (PlayerPos.y < 0)
-		{PlayerPos.y = 0;}
-		ObstableP.x = ObstableP.x - 10;
-		if (ObstableP.x < -80) 
-		{
-			Obstacle();
-		}
-		gameOver();
-		// Draw
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		DrawRectangleV(ObstableP, ObstacleS, BLACK);
-		DrawRectangleV(PlayerPos, PlayerSize , MAROON);
-		EndDrawing();
-
+		Update();
+		Draw();
 	}
-
 	CloseWindow();      
 	return 0;
 }
@@ -52,4 +42,60 @@ void Obstacle()
 void gameOver()
 {
 	bool CheckCollisionRecs(Rectangle rec1, Rectangle rec2);                                            
+}
+void Draw()
+{
+	BeginDrawing();
+	ClearBackground(RAYWHITE);	
+	DrawTextureRec(ImagenFondo, fondo, position1, WHITE);
+	DrawTextureRec(ImagenFondo, fondo2, position2, WHITE);
+	DrawTextureRec(ImagenFondo, fondo, position3, WHITE);
+	DrawRectangleV(ObstableP, ObstacleS, BLACK);
+	DrawRectangleV(PlayerPos, PlayerSize, MAROON);
+	EndDrawing();
+}
+
+void Update()
+{
+	if (IsKeyDown(KEY_UP)) PlayerPos.y -= 2.0f;
+	if (IsKeyDown(KEY_DOWN)) PlayerPos.y += 2.0f;
+	if (PlayerPos.y > 420)
+	{
+		PlayerPos.y = 420;
+	}
+	if (PlayerPos.y < 0)
+	{
+		PlayerPos.y = 0;
+	}
+	ObstableP.x = ObstableP.x - 10;
+	if (ObstableP.x < -80)
+	{
+		Obstacle();
+	}
+	position1.x += 80 * GetFrameTime();
+	if (position1.x > 1000)
+	{
+		position1.x = position3.x-fondo.width*2;
+	}
+	position2.x += 80 * GetFrameTime();
+	if (position2.x > 1000)
+	{
+		position2.x = position1.x- fondo.width*2;
+	}
+	position3.x += 80 * GetFrameTime();
+	if (position3.x > 1000)
+	{
+		position3.x = position2.x- fondo.width*2;
+	}
+	gameOver();
+}
+void Init()
+{
+
+	InitWindow(screenWidth, screenHeight, "SideScroll Practice");
+	SetTargetFPS(60);	
+	image = LoadImage("res/parallax.png");    
+	ImagenFondo = LoadTextureFromImage(image);
+	fondo = { 0.0f, 0.0f, (float)ImagenFondo.width / 2, (float)ImagenFondo.height };
+	fondo2 = { 500.0f, 0.0f, (float)ImagenFondo.width / 2, (float)ImagenFondo.height };
 }
